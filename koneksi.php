f37b039bbfa49efbdb47b9bec1cbe9b90dfdc7bc
reeganaga <rega.blank@gmail.com>
2016-11-12 15:36:07 +0700

    koneksi

--- koneksi.php
+++ koneksi.php
@@ -4,6 +4,6 @@ $user="root";
 $pass="root";
 $db="inv";
 
-$koneksi=mysqli_connect($hostname, $user, $pass,$db);
-// $koneksi_db=mysql_select_db($db);
+$koneksi=mysql_connect($hostname, $user, $pass);
+$koneksi_db=mysql_select_db($db);
 ?>
